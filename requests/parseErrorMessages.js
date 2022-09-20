c6bfb84b3e192c58ff5e222e0131821e41053628
aslam <47080903+aslam-c@users.noreply.github.com>
2022-09-20 17:08:52 +0530

    validator messages format

--- requests/parseErrorMessages.js
+++ requests/parseErrorMessages.js
@@ -5,7 +5,7 @@ module.exports = {
         for (const detail of errorDetails) {
             const fieldName = detail?.context?.label
             let message = detail?.message?.replaceAll(/"/g, '')
-            validationMessages.push([{ [fieldName]: message }])
+            validationMessages.push({ [fieldName]: message })
         }
         return validationMessages
     }
