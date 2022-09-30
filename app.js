183659adc0045b8ec84e61b5486e8b09c83bf6cf
arjun.p <arjun.p@webandcrafts.in>
2022-09-30 11:38:58 +0530

    mongodb-not comleted

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const multer=require('multer');
+const multer = require('multer');
+const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 //routes
 const indexRouter = require('./routes/index');
@@ -27,12 +30,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -40,19 +43,43 @@ app.use(function(err, req, res, next) {
   // render the error page
 
   if (err instanceof multer.MulterError) {
-   console.log("MULTER ERROR "+JSON.stringify(err)) 
+    console.log("MULTER ERROR " + JSON.stringify(err))
   }
 
   // res.status(err.status || 500);
   // res.render('error');
-  res.status(403).json({message:err.message})
+  res.status(403).json({ message: err.message })
 
 
 });
 
-const port=process.env.APP_PORT || 3000
+console.log([process.env.APP_PORT]);
+const port = process.env.APP_PORT || 3001;
+
+let mongoDbUrl = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+
+mongoose.connect(mongoDbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  auth: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS
+  },
+}, (err) => {
+  if (err) console.log("mongoose error: ", err)
+  console.log(["DB connected"])
+  app.listen({
+    port: process.env.PORT,
+    host: process.env.HOST
+  }, (res) => {
+    console.log(["Secure server started at: ", port]);
+  })
+});
+
+
 
 app.listen(port, () => {
+  console.log(['ffff==>', port])
   console.log(`Listening on port ${port}`)
 })
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,34 +1,46 @@
-const {validateRequest}= require("../requests/generateToken")
+const { validateRequest } = require("../requests/generateToken")
 const { makeJsonResponse } = require("../utils/response")
+const { eventBus } = require('js-event-bus')();
 
 module.exports = {
-    generateToken : (req, res, next) => {
+
+    check: (req, res, next) => {
+        // const findResult = await orders.find({
+        //     name: "Lemony Snicket",
+        //     date: {
+        //         $gte: new Date(new Date().setHours(00, 00, 00)),
+        //         $lt: new Date(new Date().setHours(23, 59, 59)),
+        //     },
+        // });
+    },
+
+    generateToken: (req, res, next) => {
         let httpStatusCode = 422
         let response = {}
         try {
             validateRequest(req.body)
             const token = "890290CV89"
 
-            httpStatusCode=200
-            response = makeJsonResponse("Token generated successfully", { token }, {},httpStatusCode)
+            httpStatusCode = 200
+            response = makeJsonResponse("Token generated successfully", { token }, {}, httpStatusCode)
             res.status(httpStatusCode).json(response)
         }
         catch (error) {
-            response = makeJsonResponse("Token cant be generated", {}, error,httpStatusCode,false)
+            response = makeJsonResponse("Token cant be generated", {}, error, httpStatusCode, false)
             res.status(httpStatusCode).json(response)
         }
     },
     uploadFile: (req, res, next) => {
-        let httpStatusCode=403
+        let httpStatusCode = 403
         let response = {}
         try {
-            httpStatusCode=200
-            response = makeJsonResponse("file uploaded successfully", {} , {},httpStatusCode)
+            httpStatusCode = 200
+            response = makeJsonResponse("file uploaded successfully", {}, {}, httpStatusCode)
             res.status(httpStatusCode).json(response)
         }
         catch (error) {
             console.log("Cant upload file")
-            response = makeJsonResponse("File cant be uploaded", {}, error,httpStatusCode,false)
+            response = makeJsonResponse("File cant be uploaded", {}, error, httpStatusCode, false)
             res.status(httpStatusCode).json(response)
         }
     }
